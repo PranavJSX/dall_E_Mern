16366da537e008309df2350a39be712764ef7c88
manuo <ojhapranav1999@gmail.com>
2024-10-23 23:14:01 +0530

    updating the home component to add the basic layout

--- Client/vite-project/src/App.jsx
+++ Client/vite-project/src/App.jsx
@@ -22,20 +22,19 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
-      <header className='absolute bottom-0 left-0 h-16 w-16 ...'>
+      <header className='w-full flex justify-between items center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
       <Link to='/'>
-      <div className=''>here comes the open ai logo</div>
+      <div id='openai_logo'></div>
       </Link>
       <Link to="/create-post">
-      <div>create button here</div>
+      <div className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</div>
       </Link>
-      <main><Routes>
+      </header>
+      <main className='sm:p-8 px-4 py-2 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'><Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/create-post' element={<CreatePost/> }/>
         </Routes></main> 
-      </header>
       </BrowserRouter>
-      <h1 className='text-teal-400'>fasdkfmasldkm</h1>
     </>
   )
 }
