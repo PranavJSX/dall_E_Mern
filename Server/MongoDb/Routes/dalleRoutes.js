32a2ce7b9ef68b40aa996a55476a13b4a81aebb3
manuo <ojhapranav1999@gmail.com>
2024-11-04 23:27:28 +0530

    Updating the routes after image generation has been completed and the backened is successfully working

--- Server/MongoDb/Routes/dalleRoutes.js
+++ Server/MongoDb/Routes/dalleRoutes.js
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Router } from 'express';
 import * as dotnev from 'dotenv';
+import fetch from "node-fetch";
 
 import { OpenAI} from 'openai'
 
@@ -12,8 +13,30 @@ const openai = new OpenAI({
 })
 
 
+console.log(process.env.OPENAI_API_KEY);
+
+
+
 router.route('/').get((req,res)=>{
     res.send('Hello from DALLE')
 });
+
+router.route('/').post(async(req,res)=>{
+
+    try{
+        const {prompt} = req.body;
+        const url = `https://image.pollinations.ai/prompt/${prompt}`;
+        const AI_response = await fetch(url,{
+            method:'GET',
+            width: 800,
+            height: 600,
+        });
+        console.log(AI_response.url);
+        res.status(200).json({url:AI_response.url});
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
 
 export default router;
